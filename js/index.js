ec9382651bca0d9c2236385d9017b313312e77d2
Korver <korver2017@gmail.com>
2018-07-24 00:01:49 +0800

    import bootstrap4 and set card layout

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
 let first = document.querySelector('.first');
 let second = document.querySelector('.second');
 let timer = document.querySelector('.timer');
+let image = document.querySelector('img');
 
 function callApi() {
   return new Promise((resolve, reject) => {
     if (true) {
       resolve(fetch('https://randomuser.me/api/', {})
         .then((response) => {
-          console.log(response);
+          // console.log(response);
           return response.json();
         }).then((jsonData) => {
-          console.log(jsonData);
-          console.log(jsonData.results[0].email);
+          // console.log(jsonData);
+          // console.log(jsonData.results[0].email);
           first.innerText = jsonData.results[0].email;
         }).catch((err) => {
           console.log('錯誤:', err);
@@ -23,17 +24,27 @@ function callApi() {
   })
 }
 callApi().then(setTimeout(() => {
-  console.log('Time Up!');
   timer.innerText = '2s Time Up!';
-  fetch('https://randomuser.me/api/', {})
+  fetch('https://dog.ceo/api/breeds/image/random', {})
     .then((response) => {
-      console.log(response);
+      // console.log(response);
       return response.json();
     }).then((jsonData) => {
-      console.log(jsonData);
-      console.log(jsonData.results[0].email);
+      // console.log(jsonData);
+      // console.log(jsonData.message);
+      image.src = jsonData.message;
+    }).catch((err) => {
+      console.log('錯誤:', err);
+    });
+  fetch('https://randomuser.me/api/', {})
+    .then((response) => {
+      // console.log(response);
+      return response.json();
+    }).then((jsonData) => {
+      // console.log(jsonData);
+      // console.log(jsonData.results[0].email);
       second.innerText = jsonData.results[0].email;
     }).catch((err) => {
       console.log('錯誤:', err);
     })
-}, 2000))
+}, 2000));
