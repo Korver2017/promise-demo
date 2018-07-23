253eb66a95529ff62eb600dc42af2b75bdcfc411
Korver <korver2017@gmail.com>
2018-07-23 22:47:18 +0800

    call another api after callApi function

--- js/index.js
+++ js/index.js
@@ -21,4 +21,17 @@ function callApi() {
     }
   })
 }
-callApi();
+callApi().then(setTimeout(() => {
+  console.log('Time Up!');
+  fetch('https://randomuser.me/api/', {})
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    }).then((jsonData) => {
+      console.log(jsonData);
+      console.log(jsonData.results[0].email);
+      second.innerText = jsonData.results[0].email;
+    }).catch((err) => {
+      console.log('錯誤:', err);
+    })
+}, 2000))
